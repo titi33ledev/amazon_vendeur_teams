54c5f470b7547288f0c65174540eb79174541b50
titi33ledev <tidiane.hubert.bordeaux@gmail.com>
2024-11-22 10:05:27 +0100

    Remettre à 0

--- scrap_module.py
+++ scrap_module.py
@@ -4,32 +4,41 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
-
 #%% Aller sur Amazon et chercher le mot clés voulu et on récupère le code entier de la page
 def scrap(mot_cles):
-    driver = webdriver.Chrome()
-    driver.get("https://www.amazon.fr/")
     
-    # Accepter les cookies
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="sp-cc-accept"]'))).click()
+    chrome_options = Options()
     
-    # Effectuer la recherche
-    search_bar = driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]')
-    search_bar.send_keys(mot_cles)
-    search_bar.send_keys(Keys.RETURN)
+    # Initialiser le driver avec les options configurées
+    driver = webdriver.Chrome(options=chrome_options)
     
-    # Attendre le chargement des résultats
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "s-main-slot")))
+    try:
+        # Naviguer vers Amazon
+        driver.get("https://www.amazon.fr/")
+        
+        # Accepter les cookies
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="sp-cc-accept"]'))).click()
+        
+        # Effectuer la recherche
+        search_bar = driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]')
+        search_bar.send_keys(mot_cles)
+        search_bar.send_keys(Keys.RETURN)
+        
+        # Attendre le chargement des résultats
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "s-main-slot")))
+        
+        # Récupérer le HTML
+        page_source = driver.page_source
+    finally:
+        driver.quit()  # Toujours fermer le driver, même en cas d'erreur
     
-    # Récupérer le HTML
-    page_source = driver.page_source
-    driver.quit()
     return BeautifulSoup(page_source, 'html.parser')
 
 #%% Récupérer les liens qui renvoit vers les produits et stocker dans une liste
