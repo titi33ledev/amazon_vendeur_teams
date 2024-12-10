1311bd9089531eef952ed573f13401a1a76cc462
titi33ledev <tidiane.hubert.bordeaux@gmail.com>
2024-12-10 17:06:44 +0100

    modification dashboard

--- dashboard_test.py
+++ dashboard_test.py
@@ -61,6 +61,7 @@ if mot_cles and mot_cles != '...':
             # Étape 4 : Extraire les informations des vendeurs
             st.info("Récupération des informations des vendeurs...")
             seller_data = extract_seller_data(seller_links, limit=50)
+            print(f"Liens sellers trouvées : {seller_data}")
             if not seller_data:
                 st.warning("Aucune information vendeur trouvée.")
                 st.stop()
